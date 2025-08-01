ac2600ae7ec0b24ecdd3298ec9cd17b0b888ce00
Akhil <akhil.dhawan@successive.tech>
2025-08-01 13:14:16 +0530

    assessment

--- src/Utils/Seed.ts
+++ src/Utils/Seed.ts
@@ -1,1 +1,13 @@
 import { Student } from "../Models/UserSchema"
+
+const data = [{
+    'name': 'akhil',
+    'age': 15,
+    'grade': 'A',
+    'email': 'akhil@example.com'
+}]
+
+export const Seed = () => {
+    // const value = new Student(data);
+    const result = await Student.insertOne(data)
+}
