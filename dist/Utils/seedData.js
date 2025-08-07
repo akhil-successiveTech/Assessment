b7cfdb8097ff4c009bdfd834b1b266d48b10fb94
Akhil <akhil.dhawan@successive.tech>
2025-08-07 15:41:23 +0530

    assessment

--- dist/Utils/seedData.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.seedData = void 0;
-exports.seedData = [
-    { 'id': 1, 'name': 'akhil' },
-    { 'id': 2, 'name': 'parth' }
-];
